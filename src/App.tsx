55626ac10a2bb3b7f54c6e7a01c99a56c1e68fee
felipetodev <fe.ossandon.u@gmail.com>
2024-05-07 21:17:24 -0400

    feat: add header and link to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { HandshakeIcon } from "lucide-react";
+import { Header } from "./components/header";
 import { FormTable } from "./components/form-table";
 
 export default function App() {
   return (
-    <main className="max-w-7xl mx-auto">
-      <header className="h-[80px] flex items-center border-b mb-4">
-        <HandshakeIcon className="w-8 h-8 text-primary mr-4" />
-        <h1 className="text-2xl font-semibold text-primary">Nazar Management Tool</h1>
-      </header>
+    <main className="max-w-7xl mx-auto px-6">
+      <Header />
       <FormTable />
     </main>
   )
